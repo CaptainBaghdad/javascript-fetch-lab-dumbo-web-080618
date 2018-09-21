7ae0e02a59ce98b80f06c53e8ff32d74755bf4b4
Jason Pitts <juniorjr083@gmail.com>
2018-09-21 14:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
+const token = '042b85ea60406baf15fe36697190d004acf5f15e';
+ const repo = 'learn-co-curriculum';
+ const title = 'javascript-fetch-lab';
+ const baseUrl = '/api.gtihub.com';
+ const gitName = 'CaptainBaghdad';
+ const forkUrl = `${gitName}/javascript-fetch-lab`;
+const repoTitle = 'javascript-fetch-lab';
+
+
 function getIssues() {
+ 
 }
 
 function showIssues(json) {
 }
 
-function createIssue() {
+
 }
 
 function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+ 
   //use fetch to fork it!
+  fetch(`/api.github.com/repos/${repo}/${title}/forks`,{ 
+    method: 'post',
+    headers: {
+      Authorization : `token ${token}`
+    }
+    
+  })
+  .then(res => res.json())
+  .then(json => console.log(json));
+  
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '';
 }
